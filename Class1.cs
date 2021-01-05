07020fd21fc5b3fcfced3d77a9f038eb7b46290f
osalysdecatarina <72741076+osalysdecatarina@users.noreply.github.com>
2021-01-05 16:31:19 +0100

    Ajoutez des fichiers projet.

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercice_421
+{
+    class De
+    {
+        private int valeur;
+        private int jet = 0;
+        //constructeurs 
+        public void de()
+        {
+            int De = 0;
+            De++;
+        }
+        //Methodes
+        public int lancer()
+        {
+            jet++;
+            Random aleatoire = new Random();
+            valeur = aleatoire.Next(1, 7);
+            return valeur;
+        }
+
+    }
+    class manche421
+    {
+        int nombre = 0;
+        public int demarrer()
+        {
+            nombre++;
+            return nombre;
+        }
+    }
+    class Partie
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercice_421
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lancers;
+            int capitalPoints;
+            Console.WriteLine("Bienvenue dans le jeu du 421 \nVeuillez choisir le nombre de lancers que vous souhaitez faire");
+            lancers = int.Parse(Console.ReadLine());
+            capitalPoints = lancers * 10;
+            Console.WriteLine("Vous avez " + capitalPoints + "points");
+            manche421 m1 = new manche421();
+            m1.demarrer();
+            
+
+        }
+    }
+}
